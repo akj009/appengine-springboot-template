f30876978c79a60cc9f638de313a961e6b69e046
Abhishek Jha <a.jha@vuclip.com>
2019-04-18 13:09:17 +0530

    initial files

---
+++ src/main/java/com/vuclip/insights/appengine/AppEngineServiceApplication.java
@@ -0,0 +1,19 @@
+package com.vuclip.insights.appengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class AppEngineServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppEngineServiceApplication.class, args);
+    }
+}
